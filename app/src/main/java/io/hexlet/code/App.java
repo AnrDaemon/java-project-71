7cec44d9fd3f3f074056b123383782b218b4739e
Andrey Repin <anrdaemon@yandex.ru>
2025-06-05 09:59:06 +0300

    Added commandline parameters

--- app/src/main/java/io/hexlet/code/App.java
+++ app/src/main/java/io/hexlet/code/App.java
@@ -4,9 +4,21 @@ import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0", description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String left;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String right;
+
+    @Option(paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]", names = {
+            "-f", "--format" })
+    private String outputFormat;
 
     public String getGreeting() {
         return "Hello World!";

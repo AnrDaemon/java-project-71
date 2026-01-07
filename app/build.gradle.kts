a12c11cd6b2b8961275dba8192ea8983afa38f6f
Andrey Repin <anrdaemon@yandex.ru>
2026-01-07 15:04:29 +0300

    Attempted buid fix #4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,10 @@ checkstyle {
     maxErrors = 0
 }
 
+jacoco {
+    toolVersion = "0.8.13"
+}
+
 sonar {
     properties {
         property("sonar.projectKey", "AnrDaemon_java-project-71")
@@ -76,6 +80,7 @@ application {
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jar {
@@ -95,6 +100,7 @@ tasks.withType<JavaCompile> {
 }
 
 tasks.jacocoTestReport {
+    dependsOn(tasks.test)
     reports {
         xml.required.set(true)
         csv.required.set(false)
